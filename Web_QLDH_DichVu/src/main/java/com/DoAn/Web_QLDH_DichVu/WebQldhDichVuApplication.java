aea69e65d87c212e99941d4593a712b268399a92
Taibaby-18 <ngotantai18092004@gmail.com>
2026-03-21 20:32:58 +0700

    Initial Project Structure: Entity and Repository

---
+++ Web_QLDH_DichVu/src/main/java/com/DoAn/Web_QLDH_DichVu/WebQldhDichVuApplication.java
@@ -0,0 +1,13 @@
+package com.DoAn.Web_QLDH_DichVu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebQldhDichVuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebQldhDichVuApplication.class, args);
+	}
+
+}
---
+++ Web_QLDH_DichVu/src/test/java/com/DoAn/Web_QLDH_DichVu/WebQldhDichVuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DoAn.Web_QLDH_DichVu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebQldhDichVuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
